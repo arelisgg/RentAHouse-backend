2e82dc991d96ad262b672177b9d425f10d07497a
Arelis-cujae <gonzalezgarciaarelis@gmail.com>
2021-11-11 00:10:04 -0430

    General Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
+import { HostModule } from './host/host.module';
+import { HouseModule } from './house/house.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { MinioModule } from './minio/minio.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/CasaDeRenta', {
-      useFindAndModify: false,
+    MongooseModule.forRoot('mongodb://localhost/RentAHouse'),
+    MulterModule.register({
+      dest: './files',
     }),
     GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+    HouseModule,
+    MinioModule,
   ],
 })
 export class AppModule {}
---
+++ src/house/model/facility.class.ts
@@ -0,0 +1,3 @@
+export class Facility {
+    name: String;
+}
